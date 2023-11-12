17473752bcb38aa550cd82d346239a467609c51d
aisharoslan <ar7805@nyu.edu>
2023-11-12 14:58:38 -0500

    added containsDuplicate code

--- Week 1/ContainsDuplicate.py
+++ Week 1/ContainsDuplicate.py
@@ -1,1 +1,14 @@
 #Easy
+
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        hash_set = set()
+        for num in nums: #O(n)
+            if num in hash_set:
+                return True
+            else:
+                hash_set.add(num) #O(1)
+        return False
+
+#Time: O(n)
+#Space: O(n)
