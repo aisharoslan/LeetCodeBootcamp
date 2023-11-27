38376ca7118542ae0ea61049b9e8908790b71cfb
aisharoslan <ar7805@nyu.edu>
2023-11-27 10:36:34 -0500

    findcenterofstargraph

--- Week 4/FindCenterOfStarGraph.py
+++ Week 4/FindCenterOfStarGraph.py
@@ -1,1 +1,28 @@
 #Easy
+class Solution:
+    def findCenter(self, edges: list[list[int]]) -> int:
+        for i in edges[0]:
+            if i in edges[1]:
+                return i
+# #Time: O(n)
+# #Space: O(1)
+
+class Solution:
+    def findCenter(self, edges: list[list[int]]) -> int:
+        if edges[0][0] in edges[1]: 
+            return edges[0][0]
+        return edges[0][1]  
+#Time: O(1)
+#Space: O(1)      
+
+class Solution:
+    def findCenter(self, edges: list[list[int]]) -> int:
+        firstEdge = edges[0]  
+        secondEdge = edges[1]   
+
+        elem1, elem2 = firstEdge #each edge[i] has length 2 only
+        if elem1 in secondEdge:
+            return elem1  
+        return elem2
+#Time: O(2) = O(1) #max times is 2 to check for existence
+#Space: O(1)
